aad46eb3cef0de9d6b989a1ca75ffd76f91ed52a
Deepakkv <deepakkv@in-deepakkv.local>
2017-08-10 10:29:25 +0530

    [Deepak] app pg connection string update for Heroku env

--- app/index.js
+++ app/index.js
@@ -29,7 +29,8 @@ app.get('/', (req, res) => {
 app.post('/users', (req, res) => {
     var user = req.body;
 
-    var db = pgp('postgres://admin:C0mplexPwd!1234@localhost:5432/node_hero');
+    // var db = pgp('postgres://admin:C0mplexPwd!1234@localhost:5432/node_hero');
+    var db = pgp('postgres://aolzbgrcbpwkch:a44c5f992a07d2c13f4454507d971ada9cf221f8a3e4a73313855ffb51b5897a@ec2-23-21-220-48.compute-1.amazonaws.com:5432/ddod315al975fg');
     db.none('INSERT INTO users (name, age) VALUES ($1, $2);', [user.name, user.age]).then(() => {
         res.status(200).json({
             status: 'success',
