1260ee3a6b621cbd6624d37c546c9850f4051d67
Deepakkv <deepakkv@in-deepakkv.local>
2017-09-14 01:36:33 +0530

    [Deepak] aws s3 upload and url fetch.

--- app/upload/upload.server.controller.js
+++ app/upload/upload.server.controller.js
@@ -1,12 +1,15 @@
 const AWS = require('aws-sdk');
 const async = require('async');
 const bucketName = "medwall";
-const path = require('path');
 const fs = require('fs');
-let pathParams, image, imageName;
+const config = require('../../config/env/development');
+const uuidv1 = require('uuid/v1');
+
+const getFileExtension = (filename) => {
+    return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+};
 
 AWS.config.loadFromPath('config.json');
-
 
 const s3 = new AWS.S3({region: 'ap-south-1'});
 const createMainBucket = (callback) => {
@@ -34,9 +37,9 @@ const createMainBucket = (callback) => {
 const createItemObject = (callback) => {
     const params = {
         Bucket: bucketName,
-        Key: `${imageName}`,
+        Key: `${fileName}`,
         ACL: 'public-read',
-        Body: image
+        Body: file
     };
     s3.putObject(params, function (err, data) {
         if (err) {
@@ -50,14 +53,16 @@ const createItemObject = (callback) => {
 };
 exports.upload = (req, res, next) => {
     var tmp_path = req.files.file.path;
-    image = fs.createReadStream(tmp_path);
-    imageName = req.files.file.name;
+    file = fs.createReadStream(tmp_path);
+    fileType = req.files.file.type;
+    fileExtension = getFileExtension(req.files.file.name);
+    fileName = uuidv1().concat('.', fileExtension);
     async.series([
         createMainBucket,
         createItemObject
     ], (err, result) => {
         if (err) return res.send(err);
-        else return res.json({message: "Successfully uploaded"})
+        else return res.json({message: "Successfully uploaded", url: 'https://s3.' + config.region + '.amazonaws.com/' + config.bucket + '/' + fileName})
     })
 };
 exports.displayForm = (req, res) => {
