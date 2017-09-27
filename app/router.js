28d5003b81d9346907b4d742f0edaf8cb639c153
Deepakkv <deepakkv@in-deepakkv.local>
2017-09-27 12:37:21 +0530

    [Deepak] Amazon S3 Upload endpoint

--- app/router.js
+++ app/router.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const multipart = require('connect-multiparty');
 const neo = require('./neo/person');
 const constants = require('./constants/constants');
 const jwt = require('./auth/jwt');
 const payload = require('./response/payload');
+const Upload = require('./upload/upload.server.controller');
 
+const multipartMiddleware = multipart();
 const router = express.Router();
 const secureRouter = express.Router();
 
@@ -17,6 +19,8 @@ router.get('/', getAppStatus);
 // neo4j access points
 router.post('/api/v1/login', neo.login);
 router.post('/api/v1/register', neo.register);
+router.get('/uploadform', Upload.displayForm);
+router.put('/upload', multipartMiddleware, Upload.upload);
 
 // Validation Middleware
 secureRouter.use('', jwt.token);
---
+++ app/upload/upload.server.controller.js
@@ -0,0 +1,83 @@
+const AWS = require('aws-sdk');
+const async = require('async');
+const bucketName = "medwall";
+const fs = require('fs');
+const config = require('../../config/env/development');
+const uuidv1 = require('uuid/v1');
+
+const getFileExtension = (filename) => {
+    return filename.slice((filename.lastIndexOf(".") - 1 >>> 0) + 2);
+};
+
+AWS.config.loadFromPath('config.json');
+
+const s3 = new AWS.S3({region: 'ap-south-1'});
+const createMainBucket = (callback) => {
+    // Create the parameters for calling createBucket
+    const bucketParams = {
+        Bucket: bucketName
+    };
+    s3.headBucket(bucketParams, function (err, data) {
+        if (err) {
+            console.log("ErrorHeadBucket", err);
+            s3.createBucket(bucketParams, function (err, data) {
+                if (err) {
+                    console.log("Error", err);
+                    callback(err, null)
+                } else {
+                    callback(null, data)
+                }
+            });
+        } else {
+            callback(null, data)
+        }
+    })
+};
+
+const createItemObject = (callback) => {
+    const params = {
+        Bucket: bucketName,
+        Key: `${fileName}`,
+        ACL: 'public-read',
+        Body: file
+    };
+    s3.putObject(params, function (err, data) {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, data)
+        }
+    })
+};
+exports.upload = (req, res, next) => {
+    var tmp_path = req.files.file.path;
+    file = fs.createReadStream(tmp_path);
+    fileType = req.files.file.type;
+    fileExtension = getFileExtension(req.files.file.name);
+    fileName = uuidv1().concat('.', fileExtension);
+    async.series([
+        createMainBucket,
+        createItemObject
+    ], (err, result) => {
+        if (err) return res.send(err);
+        else return res.json({
+            fileName: req.files.file.name,
+            fileType: req.files.file.type,
+            fileSize: req.files.file.size,
+            message: "Successfully uploaded",
+            url: 'https://s3.' + config.region + '.amazonaws.com/' + config.bucket + '/' + fileName
+        })
+    })
+};
+exports.displayForm = (req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+    res.write(
+        '<form action="/upload" method="post" enctype="multipart/form-data">' +
+        '<input type="file" name="file">' +
+        '<input type="submit" value="Upload">' +
+        '</form>'
+    );
+    res.end();
+};
